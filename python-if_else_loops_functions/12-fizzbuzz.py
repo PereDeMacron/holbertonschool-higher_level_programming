3d5542781fd40335391957552f617de84efbe40b
root <peredemacron@gmail.com>
2023-09-28 08:42:06 +0000

    pycode style all

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0):
-            print ('Fizz ', end='')
+            print('Fizz ', end='')
         elif (i % 5 == 0):
-            print ('Buzz ', end='')
+            print('Buzz ', end='')
         else:
-            print (f"{i} ", end='')
+            print(f"{i} ", end='')

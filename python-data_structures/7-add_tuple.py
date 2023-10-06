384c0d8113caa4f592588cac5fbf146688e7c2f8
PereDeMacron <80179483+PereDeMacron@users.noreply.github.com>
2023-10-06 15:00:41 +0200

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2]
-    b1, b2 = tuple_b[:2]
-    
+    a1, a2 = tuple_a[:2] if len(tuple_a) >= 2 else (0, 0)
+    b1, b2 = tuple_b[:2] if len(tuple_b) >= 2 else (0, 0)
     sum_1 = a1 + b1
     sum_2 = a2 + b2
-    
-    return (sum_1, sum_2)
+    result_tuple = (sum_1, sum_2)
+
+    return result_tuple

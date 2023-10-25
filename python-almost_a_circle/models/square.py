dda1f5a72056a5da1aa730bc1b5b0320b8886452
peredemacron <peredemacron@gmail.com>
2023-10-25 13:35:01 +0000

    1

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,88 +1,73 @@
 #!/usr/bin/python3
-
+""" Module that contains class Square,
+inheritance of class Rectangle
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Class to represent a square and perform operations on it.
-
-    Attributes:
-        None
-    """
+    """ Class Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square object.
-
-        Args:
-            size (int): Length of the square's side.
-            x (int): X-coordinate of the top-left corner.
-            y (int): Y-coordinate of the top-left corner.
-            id (int): Object identifier (optional).
-        """
+        """ Initializes instances """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str special method """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
 
     @property
     def size(self):
-        """
-        Returns the length of the square's side
-        (equivalent to the width of the rectangle).
-        """
+        """ Getter size """
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        Sets the length of the square's side
-        (equivalent to the width and height of the rectangle).
-
-        Args:
-            value (int): New length of the side.
-        """
+        """ Setter size """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """
-        Returns a string representation of the square.
-        """
-        return "[Square]\
-        ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        """ str special method """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
 
     def update(self, *args, **kwargs):
-        """
-        Assigns arguments to attributes, supports both
-        arguments and key-value pairs.
-
-        Args:
-            *args: Arguments to update the attributes
-            (in the order: id, size, x, y).
-            **kwargs: Key-value pairs to update the attributes.
-        """
-        if args:
-            attrs = ["id", "size", "x", "y"]
+        """ update method """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
-                setattr(self, attrs[i], args[i])
-            self.width = self.size
-            self.height = self.size
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
         else:
             for key, value in kwargs.items():
-                setattr(self, key, value)
-                if key == "size":
-                    self.width = value
-                    self.height = value
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """
-        Returns a dictionary representing the Square object.
+        """ Returns a dictionary with attributes """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
 
-        Returns:
-            dict: Dictionary with the object's attributes.
-        """
-        return {
-            "id": self.id,
-            "size": self.size,
-            "x": self.x,
-            "y": self.y
-        }
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res

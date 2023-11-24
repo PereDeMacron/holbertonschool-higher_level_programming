57e757c74a374b7b72479ec1a81d94daeb7827a2
PereDeMacron <peredemacron@gmail.com>
2023-11-24 23:27:29 +0100

    fix 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 --creates the MySQL server user
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@ WITH GRANT OPTION;
+CREATE USER 'user_0d_1'@'localhost' IF 'user_0d_1'@'localhost' NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
